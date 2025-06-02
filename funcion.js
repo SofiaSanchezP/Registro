2df82a0d3883d7447a9ddd99d187a78cc73e7bb6
SofiaSanchezP <129930051+SofiaSanchezP@users.noreply.github.com>
2025-06-02 19:36:35 -0300

    Update funcion.js

--- funcion.js
+++ funcion.js
@@ -1,19 +1,25 @@
-function validarRegistro(usuario, email, password, confirmPassword) {
-    if (usuario === "" || email === "" || password === "" || confirmPassword === "") {
-     return "⚠️ Todos los campos son obligatorios.";
-    }
+function validarRegistro() {
+  const nombre = prompt("Ingresa tu nombre:");
+  const email = prompt("Ingresa tu email:");
+  const mensaje = prompt("Ahora ingresa tu mensaje:");
 
-    if (!email.includes("@") || !email.includes(".")) {
-     return "⚠️ El correo electrónico no es válido.";
-    }
+  console.log("Nombre:", nombre);
+  console.log("Email:", email);
+  console.log("Mensaje:", mensaje);
 
-    if (password.length < 6) {
-     return "⚠️ La contraseña debe tener al menos 6 caracteres.";
-    }
-
-    if (password !== confirmPassword) {
-     return "⚠️ Las contraseñas no coinciden.";
-    }
-
-    return "✅ Registro exitoso. ¡Bienvenido/a, " + usuario + "!";
+  if (nombre === "") {
+    alert("⚠️ Por favor ingresa tu nombre");
+  } else if (email === "") {
+    alert("⚠️ Por favor ingresa tu correo electrónico");
+  } else if (!email.includes("@") || !email.includes(".")) {
+    alert("⚠️ Por favor ingresa un correo válido");
+  } else if (password === "") {
+    alert("⚠️ Por favor ingresa una contraseña");  
+  } else if (password.length < 6) {
+    alert("⚠️ La contraseña debe tener al menos 6 caracteres.");
+  } else if (password !== confirmPassword) {
+    alert("⚠️ Las contraseñas no coinciden.");
+  } else {
+    alert("✅ Registro exitoso. ¡Bienvenido/a, " + usuario + "!");
+  }
 }
